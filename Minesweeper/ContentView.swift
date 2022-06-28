bc73a938c08d4c87bd1e4a0a2c2ac722ba49e596
EMCain <11471197+EMCain@users.noreply.github.com>
2022-06-27 19:10:18 -0700

    First commit: Create sample grid layout

--- Minesweeper/ContentView.swift
+++ Minesweeper/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Tile: View {
+    let row: Int
+    let col: Int
+    let content: String
+    let squareSize: CGFloat = CGFloat(50.0)
+    let isHidden: Bool = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            let square = Rectangle().aspectRatio(1, contentMode: .fit)
+            if isHidden {
+                square.foregroundColor(.blue)
+            } else {
+                square.foregroundColor(.white)
+            }
+            
+
+
+            Text(content).font(.largeTitle)
+        }
+
+            
+    }
+}
+
+
+struct ContentView: View {
+    var rows: Array<Array<String>> = [["", "?", "⛳️"], ["", "2", "💣"]]
+    
+    var body: some View {
+        VStack{
+            ForEach(Array(rows.enumerated()), id: \.offset) { rowIndex, row in
+                HStack{
+                    ForEach(Array(row.enumerated()), id: \.offset) { colIndex, content in
+                        Tile(row: rowIndex, col: colIndex, content: content)
+                    }
+                }
+            }
+        }
+        .padding(10)
+
     }
 }
 
