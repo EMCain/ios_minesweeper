16acd19c7ef308aba7ca65fe0a414e93e1dadd91
EMCain <11471197+EMCain@users.noreply.github.com>
2022-06-27 17:55:08 -0700

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Emily on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
